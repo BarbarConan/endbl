133ad581a2a06ff97aeb4ee098662d2ab3f05e64
YKM\senolk <senolk@lidyateknoloji.com>
2017-06-02 12:41:01 +0300

    top bar

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
@@ -9,17 +20,21 @@ var sassPaths = [
 
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
+  .pipe(sourcemaps.init({loadMaps: true}))    
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
     })
+    
       .on('error', $.sass.logError))
+    
     .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
+      browsers: ['last 2 versions', 'ie >= 11']
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['sass', 'browser-sync'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
