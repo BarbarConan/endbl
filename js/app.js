3977e3c53597039028e2b3f29d4c341e2a9b2ba9
senol <msenolkeskin@gmail.com>
2017-06-05 19:29:05 +0300

    design

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-$(document).foundation();
+var $scrollTop = $('button#ScrollTop'), $doc = $(document);
+
+// Initialize Foundation.
+$doc.foundation();
+
+$doc.on('scroll', function() {
+  if ($(this).scrollTop() > 200) {
+    $scrollTop.stop().fadeIn('fast');
+  } else {
+    $scrollTop.stop().fadeOut('fast');
+  }
+});
+
+$scrollTop.on('click', function() {
+  $('html, body').animate(
+    {
+      scrollTop: 0
+    },
+    500
+  );
+});
